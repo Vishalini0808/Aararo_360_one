26bc84d4ba802dce036a8ecd206a3c04e20c8008
Bala <balasudahn17@gmail.com>
2025-10-23 18:43:42 +0530

    updated

--- babycare/babycare/src/main/java/babycare/babycare/BabycareApplication.java
+++ babycare/babycare/src/main/java/babycare/babycare/BabycareApplication.java
@@ -1,11 +1,13 @@
-package com.example.babycare;
+package babycare.babycare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BabycareApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(BabycareApplication.class, args);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabycareApplication.class, args);
+	}
+
 }
--- babycare/babycare/src/test/java/babycare/babycare/BabycareApplicationTests.java
+++ babycare/babycare/src/test/java/babycare/babycare/BabycareApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.babycare;
+package babycare.babycare;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
